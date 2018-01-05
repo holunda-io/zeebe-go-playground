1f6b9242c76f6aab32c3c5d57c874ccad38ed6ef
Patrick Schalk <patrick.schalk@holisticon.de>
2018-01-05 17:16:51 +0100

    Added task + yml process

--- src/main.go
+++ src/main.go
@@ -5,31 +5,52 @@ import (
 	"fmt"
 	"errors"
 	"encoding/json"
+	"os/signal"
+	"os"
+	"github.com/zeebe-io/zbc-go/zbc/zbmsgpack"
 )
 
 const topicName = "default-topic"
 const BrokerAddr = "127.0.0.1:51015"
-const processFile = "src/simpleProcess.bpmn"
+const processFileBpmn = "src/simpleProcess.bpmn"
+const processFileYaml = "src/simpleProcess.yml"
 const processId = "simpleProcess"
+const taskA = "task-a"
 
 var errClientStartFailed = errors.New("cannot start client")
 var errWorkflowDeploymentFailed = errors.New("creating new workflow deployment failed")
 
 func main() {
 
+	zbClient := createNewClient()
+
+	loadTopologie(zbClient)
+
+	//deployProcessBpmn(zbClient)
+	deployProcessYaml(zbClient)
+
+	startProcess(zbClient)
+
+	subscriptionCh, subscription := createSubscriptionForTaskA(zbClient)
+
+	startGoRoutineToCloseSubscriptionOnExit(zbClient, subscription)
+
+	waitForTaskAndComplete(subscriptionCh, zbClient)
+}
+
+func createNewClient() (*zbc.Client) {
+	fmt.Println("Create new zeebe client")
+
 	zbClient, err := zbc.NewClient(BrokerAddr)
 	if err != nil {
 		panic(errClientStartFailed)
 	}
 
-	loadTopologie(zbClient)
-
-	deployProcess(zbClient)
-
-	startProcess(zbClient)
+	return zbClient
 }
 
 func loadTopologie(zbClient *zbc.Client) {
+	fmt.Println("Load broker topologie")
 
 	topology, err := zbClient.Topology()
 	if err != nil {
@@ -37,19 +58,31 @@ func loadTopologie(zbClient *zbc.Client) {
 	}
 
 	b, err := json.MarshalIndent(topology, "", "    ")
-	fmt.Println(string(b))
+	fmt.Println("Topologie: ", string(b))
 }
 
-func deployProcess(zbClient *zbc.Client) {
-	response, err := zbClient.CreateWorkflowFromFile(topicName, zbc.BpmnXml, processFile)
+func deployProcessBpmn(zbClient *zbc.Client) {
+	deployProcess(zbClient, zbc.BpmnXml, processFileBpmn)
+}
+
+func deployProcessYaml(zbClient *zbc.Client) {
+	deployProcess(zbClient, zbc.YamlWorkflow, processFileYaml)
+}
+
+func deployProcess(zbClient *zbc.Client, resourceType, path string) {
+	fmt.Printf("Deploy '%s' process '%s'\n", resourceType, processFileYaml)
+
+	response, err := zbClient.CreateWorkflowFromFile(topicName, resourceType, path)
 	if err != nil {
 		panic(errWorkflowDeploymentFailed)
 	}
 
-	fmt.Println(response.String())
+	fmt.Println("Deployed Process Responce: ", response.String())
 }
 
 func startProcess(zbClient *zbc.Client) {
+	fmt.Println("Start process ", processId)
+
 	payload := make(map[string]interface{})
 	payload["somePayload"] = "31243"
 	payload["someOtherPayload"] = "lol"
@@ -61,5 +94,43 @@ func startProcess(zbClient *zbc.Client) {
 		panic(err)
 	}
 
-	fmt.Println(msg.String())
+	fmt.Println("Start Process responce: ", msg.String())
+}
+
+func createSubscriptionForTaskA(zbClient *zbc.Client) (chan *zbc.SubscriptionEvent, *zbmsgpack.TaskSubscription) {
+	fmt.Println("Open task subscription for Task A")
+
+	subscriptionCh, subscription, _ := zbClient.TaskConsumer(topicName, "lockOwner", taskA)
+	return subscriptionCh, subscription
+}
+
+func waitForTaskAndComplete(subscriptionCh chan *zbc.SubscriptionEvent, zbClient *zbc.Client) {
+	for {
+		fmt.Println("Wait for Task A")
+
+		message := <-subscriptionCh
+		fmt.Println("Message of task A subscription: ", message.String())
+
+		// complete task after processing
+		response, _ := zbClient.CompleteTask(message)
+		fmt.Println("Complete Task Responce: ", response)
+	}
+}
+
+func startGoRoutineToCloseSubscriptionOnExit(zbClient *zbc.Client, subscription *zbmsgpack.TaskSubscription) {
+	fmt.Println("Create go routine which waits for app interrrupt")
+
+	osCh := make(chan os.Signal, 1)
+	signal.Notify(osCh, os.Interrupt)
+	go func() {
+		<-osCh
+		fmt.Println("Closing subscription.")
+		_, err := zbClient.CloseTaskSubscription(subscription)
+		if err != nil {
+			fmt.Println("failed to close subscription: ", err)
+		} else {
+			fmt.Println("Subscription closed.")
+		}
+		os.Exit(0)
+	}()
 }
